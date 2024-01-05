4468cf4bff8bfadb570d28497013069312d2bd2d
beatriz1712 <beatriz1712sc@gmail.com>
2024-01-05 14:02:53 -0500

    up server con chalk

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import 'dotenv/config'
 import app from './app.js';
 
 // Inicia la aplicación
+//**** UP SERVER  */
 const PORT = 8080;
-app.listen(PORT, () => console.log('Listen puerto 8080'));
+app.listen(PORT, () => {
+  console.log(chalk.bgYellowBright.black.bold(`SERVER UP PORT: ${PORT}`));
+});
+
